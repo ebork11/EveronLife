9b10ba5ba261f341b6c47d66c5dba956dc300396
Ethan Bork <ebork11@gmail.com>
2022-06-11 20:30:43 -0600

    Added radio functionality to phone but only works if player speaks prior to picking it up rn

--- src/Scripts/Game/Features/Phones/EL_PhoneUI.c
+++ src/Scripts/Game/Features/Phones/EL_PhoneUI.c
@@ -13,7 +13,41 @@ modded class SCR_PlayerController
 	}
 	
 	void Action_OpenPhone() 
-	{
-		Print("Hello world");
+	{	
+		IEntity user = IEntity.Cast(GetGame().GetPlayerController().GetControlledEntity());
+		if (!user)
+			return;
+			
+		/*
+		SCR_InventoryStorageManagerComponent inventoryManager = SCR_InventoryStorageManagerComponent.Cast(m_MainEntity.FindComponent(SCR_InventoryStorageManagerComponent));
+		if (!inventoryManager)
+			return;
+		
+		array<IEntity> outItems = new array<IEntity>();
+		inventoryManager.GetItems(outItems);
+		
+		foreach (IEntity item : outItems)
+		{
+			if (item.GetPrefabData().GetPrefabname() == ""
+		}
+		*/
+		
+		SCR_CharacterControllerComponent characterControllerComponent = SCR_CharacterControllerComponent.Cast(user.FindComponent(SCR_CharacterControllerComponent));
+		if (!characterControllerComponent)
+			return;
+		
+		IEntity inHand = IEntity.Cast(characterControllerComponent.GetAttachedGadgetAtLeftHandSlot());
+		if (!inHand)
+			return;
+		
+		InventoryItemComponent inventoryItemComponent = InventoryItemComponent.Cast(inHand.FindComponent(InventoryItemComponent));
+		if (!inventoryItemComponent)
+			return;
+		
+		
+		
+		Print("End");
+		//characterControllerComponent.TakeGadgetInLeftHand();
+		
 	}
 }
